05405214b2d43c26630102ba623c1bd79597e30c
Yosef-ft <yosefetene02@gmail.com>
2024-09-06 03:51:43 +0300

    Script for cleaning data

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd 
+import numpy as np
 from scipy.stats import zscore
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sqlalchemy import create_engine
 
 
@@ -48,6 +51,8 @@ class DataUtils:
         missing_percent = self.data.isna().mean() * 100 
         data_types = self.data.dtypes
 
+        
+
         info_df = pd.DataFrame({
             "Missing values" : missing_values, 
             "Missing Percentage" : missing_percent, 
@@ -55,16 +60,85 @@ class DataUtils:
         })
 
         info_df = info_df[missing_percent > 0]
+        info_df = info_df.sort_values(by='Missing Percentage', ascending=False)
 
-        max_na_col = info_df.loc[info_df['Missing values'] == info_df['Missing values'].max()].index
+        max_na_col = list(info_df.loc[info_df['Missing values'] == info_df['Missing values'].max()].index)
         more_than_half_na = list(info_df.loc[info_df['Missing Percentage'] > 50].index)
 
-        print(f'The data contains {self.data.shape[0]} number of rows and {self.data.shape[1]} number of columns.\n'
-            f'The data has {info_df.shape[0]} number of missing columns\n'
-            f'The data with the maximum number of missing columns is {max_na_col}\n\n'
-            f'The data column containing more than 50% missing values are:')
-        print(*more_than_half_na, sep='\n')
+        print(f"**The data contains `{self.data.shape[0]}` rows and `{self.data.shape[1]}` columns.**\n\n"
+            f"**The data has `{info_df.shape[0]}` missing columns.**\n\n"
+            f"**The column with the maximum number of missing values is `{max_na_col}`.**\n\n"
+            f"**Columns with more than 50% missing values are:**")
+
+        # Print the list of columns with more than 50% missing values
+        for column in more_than_half_na:
+            print(f"- `{column}`")
             
         
-
         return info_df
+    
+
+    def visualize_missing_values(self):
+        '''
+        This method generates a heatmap to visually represent the missing values in the dataset.
+        '''
+
+        missing_cols = self.data.columns[self.data.isna().any()]
+
+        missing_data = self.data[missing_cols]
+
+        sns.heatmap(missing_data.T.isnull(), cbar=False)
+
+
+    def visualize_outliers(self):
+        '''
+        This funcions helps in visualizing outliers using boxplot
+        '''
+
+        numerical_cols = self.data.select_dtypes(include=['float64', 'int64']).columns
+        num_cols = len(numerical_cols)
+
+        for i in range(0, num_cols, 5):
+            fig, axes = plt.subplots(nrows = 1, ncols = 5, figsize =(20, 5))
+
+            for j, col in enumerate(numerical_cols[i: i+5]):
+                sns.boxplot(y=self.data[col], ax=axes[j])
+                axes[j].set_title(col)
+
+            plt.tight_layout()
+            plt.show()
+
+    def standardize_data(self, type: str):
+        '''
+        This function standardizes data:       
+            * This function change all columns containing ms to sec
+            * changes all columns from bytes to mega-bytes
+
+        Parameters:
+            * type(str): this specifies whether the columns to standardize are volumetric or time based
+        '''
+        
+        if type == 'time':
+            columns_to_standardize = list(self.data.columns[self.data.columns.str.contains('ms')])
+            self.data[columns_to_standardize] = self.data[columns_to_standardize] / 1000
+
+            new_cols = []
+            for item in columns_to_standardize:
+                if ')' in item:
+                    new_cols.append(item.replace('ms', 's'))
+                else:
+                    new_cols.append(item.replace('ms', '(s)'))
+        elif type == 'volume':
+            columns_to_standardize = list(self.data.columns[self.data.columns.str.contains('ms')])
+            self.data[columns_to_standardize] = self.data[columns_to_standardize] / (1024 * 1024)
+            new_cols = []
+            for item in columns_to_standardize:
+                if ')' in item:
+                    new_cols.append(item.replace('Bytes', 'MB'))
+                else:
+                    new_cols.append(item.replace('Bytes', '(MB)'))            
+
+
+        self.data.rename(columns=dict(zip(columns_to_standardize, new_cols)), inplace=True)
+
+      
