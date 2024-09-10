c06a44762f83cd8cc1a8d74f4b51933ac966af16
Yosef-ft <yosefetene02@gmail.com>
2024-09-10 17:01:46 +0300

    Multiple pages for streamlit

---
+++ app/pages/Engagement_Analysis.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from Utils import DataUtils
+from data_loader import load_dataset
+from data_loader import plot_cluster_analysis
+from data_loader import DATA
+
+
+class EngagementUI:
+    def __init__(self):
+        st.title('TellCo User Engagement Analysis')
+
+        data = DATA
+        st.write("The top 5 most engaged users", data.head())
+
+
+        data_utils = DataUtils(data)
+        cols = ['Total_sessions', 'Total_duration', 'Total_data']
+        apps = ['Social_media', 'YouTube', 'Netflix', 'Google', 'Email', 'Gaming', 'Other']
+        data, cluster_stats, kmeans = data_utils.cluster_analysis(cols, data=data, n_clusters=3)
+        data, cluster_stats_apps, kmeans = data_utils.cluster_analysis(apps, data=data, n_clusters=3)
+
+
+        plot_cluster_analysis(cols, cluster_stats)
+        plot_cluster_analysis(apps, cluster_stats_apps)
+
+
+        st.markdown("<h3 style='text-align: center;'>Analysis of application to show which is more engaging app</h3>", unsafe_allow_html=True)
+        app_metrics = data[['Social_media', 'YouTube', 'Netflix', 'Google', 'Email', 'Gaming', 'Other']]
+
+        fig = px.bar(app_metrics.max().sort_values())
+
+        st.plotly_chart(fig)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    ui = EngagementUI()
---
+++ app/pages/Satisfaciton_Analysis.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from Utils import DataUtils
+from Satisfaction_analyzer import SatAnalyzer
+
+from data_loader import load_dataset
+from data_loader import plot_cluster_analysis
+from data_loader import DATA
+
+
+class SatisfactionUI:   
+    def __init__(self):
+        st.title('TellCo User Statisfaction Analysis')
+
+        data = DATA
+        data_utils = DataUtils(data)
+
+        cols = ['Total_sessions', 'Total_duration', 'Total_data']
+        satisfacion = SatAnalyzer(cols, data, n_clusters= 3)
+        data_with_score =satisfacion.score_calculator(True)
+        cols = ['Avg_TCP', 'Avg_RTT', 'Avg_Throughput']
+        satisfacion = SatAnalyzer(cols, data, n_clusters= 3)
+        data_with_score = satisfacion.score_calculator(False)
+        data_with_score['Sataisfacion_Score'] = (data_with_score['Engagement_Score'] + data_with_score['Experience_Score']) /2 
+
+        cols = ['Engagement_Score', 'Experience_Score', 'Sataisfacion_Score']
+        scores = data_with_score[cols]
+        scores.sort_values(by='Sataisfacion_Score', ascending=False, inplace=True)
+        
+
+        features = ['Total_sessions', 'Total_duration', 'Total_data', 
+       'Social_media', 'YouTube', 'Netflix', 'Google', 'Email', 'Gaming','Other', 
+       'Avg_TCP', 'Avg_RTT',  'Avg_Throughput',
+        'Engagement_Score', 'Experience_Score', 'Sataisfacion_Score']
+
+        data_for_model = data_with_score[features]
+        cols = ['Experience_Score', 'Engagement_Score']
+        data_with_cluster, cluster_stats, kmeans = data_utils.cluster_analysis(cols, data_for_model, 2)
+
+        st.markdown("<h3 style='text-align: center;'>Average Experience and Engagement score</h3>", unsafe_allow_html=True)
+        plot_cluster_analysis(cols, cluster_stats)
+        
+        st.markdown("<h3 style='text-align: center;'>Satisfaction Score</h3>", unsafe_allow_html=True)
+        fig = px.bar(data_with_cluster.groupby(by='Cluster')[['Sataisfacion_Score', 'Experience_Score']].sum())
+        st.plotly_chart(fig)
+
+
+
+
+if __name__ == '__main__':
+    sUi = SatisfactionUI()
